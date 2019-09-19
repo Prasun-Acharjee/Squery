c5fd5595b21be33ea9164894fc5a9883d88e2ff4
Prasun-Acharjee <acharjee.prasun@gmail.com>
2019-09-19 14:36:07 +0530

    3rd commit

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,13 +2,14 @@ import React,{ Component } from "react";
 import {bindActionCreators} from 'redux';
 import ChatBot from "react-simple-chatbot";
 import { Searchbar } from "../containers/Searchbar";
+import { ThemeProvider } from 'styled-components';
 class Homepage extends Component{
      
     render(){
         const steps=[
             {
                 id: '0',
-                message: `${user.message}`,
+                message: "helloworld",
                 trigger: '1'
             },
             {
@@ -18,8 +19,8 @@ class Homepage extends Component{
               },
               {
                 id:"3",
-                message: 'Hi {previousValue}, nice to meet you!',
-                end: true,
+                message: 'Hi {previousValue}, fuck you!',
+                trigger:"1"
               }
         ];
         return(
@@ -29,7 +30,17 @@ class Homepage extends Component{
                     <h2>&nbsp;&nbsp;Frequently Asked Questions...</h2>
                 </div>
                 <div>
-                    <ChatBot steps={steps}/>
+                <ThemeProvider theme={{ background: '#f5f8fb',
+  
+  headerBgColor: '#0086c4',
+  headerFontColor: '#fff',
+  headerFontSize: '15px',
+  botBubbleColor: '#0086c4',
+  botFontColor: '#fff',
+  userBubbleColor: '#ccc8c8',
+  userFontColor: '#000000',}}>
+                    <ChatBot steps={steps} floating={true} floatingStyle={{backgroundColor:"rgba(0, 134, 196, 0.966)"}} />
+               </ThemeProvider>
                 </div>
             </div>
         )
--- src/containers/Searchbar.js
+++ src/containers/Searchbar.js
@@ -6,7 +6,7 @@ export const Searchbar=()=>{
     return(
         <div className='Searchbar-header'>
             <p style={{color:'white',fontSize:'120%'}}>Squery</p>
-            <input type='text' className='Searchbar-search' placeholder='What&apos;s your question? ' />
+            <input type='text' className='Searchbar-search' placeholder='Search your question... ' />
             <Button variant='contained' style={{backgroundColor:'rgba(219, 230, 235, 0.966)',height:'33px'}} ><img src={icon} /></Button>
             <NavLink className='Searchbar-login' to='/Volunteer' >Login as a volunteer ?</NavLink>
         </div>
